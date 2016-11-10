22b4f894b4ba7e438ed8b77a9aed5cee83810cd1
Evanzyker <quentin.rimoux@live.fr>
2016-11-10 15:29:56 -0500

    Initial commit

---
+++ app/src/main/java/mobile/informatique/cgmatane/qc/ca/phonegapcoursnumberone/MainActivity.java
@@ -0,0 +1,21 @@
+package mobile.informatique.cgmatane.qc.ca.phonegapcoursnumberone;
+
+import android.os.Bundle;
+
+import org.apache.cordova.Config;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap { /* LE BON NOM DE CLASSE!!!!! */
+    /** The view to show the ad. */
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.init();
+        super.loadUrl(Config.getStartUrl());
+        super.loadUrl("file:///android_asset/www/index.html");
+
+    }
+}
+
